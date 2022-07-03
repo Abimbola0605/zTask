b10d74e67a0e018db7ae29c8b2b4f83363e2358d
Abimbola <105178661+Abimbola0605@users.noreply.github.com>
2022-07-02 18:26:43 -0700

    Update views.py

--- link/views.py
+++ link/views.py
@@ -4,29 +4,30 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
-from rest_framework.generics import DetailAPIView
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.generics import UpdateAPIView
-from rest_framework.generics import DeleteAPIView
+from rest_framework.generics import DestroyAPIView
 from .models import link
+from .serializer import LinkSerializer
 # Create your views here.
 
 class PostListApi(ListAPIView):
-    queryst = Link.object.filter(active=True)
+    queryst = link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
 class PostCreateApi(CreateAPIView):
-    queryst = Link.object.filter(active=True)
+    queryst = link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
 class PostDetailApi(RetrieveAPIView):
-    queryst = Link.object.filter(active=True)
+    queryst = link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
 class PostUpdateApi(UpdateAPIView):
-    queryst = Link.object.filter(active=True)
+    queryst = link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
 class PostDeleteApi(DestroyAPIView):
-    queryst = Link.object.filter(active=True)
+    queryst = link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
